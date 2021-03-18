f728e238ce96cf5052dd6f0651dd03fe51411766
aboell <andy.boell@gmail.com>
2021-03-18 10:23:52 -0500

    Update certbot2lightspeed.bash

--- certbot2lightspeed.bash
+++ certbot2lightspeed.bash
@@ -4,25 +4,37 @@
 # 
 # Required parameter(s) in /opt/scripts/.environmentFile to be populated
 
+# Flag to indicate last time script was ran
+echo $(id) > /opt/lastexecuted
+
 # Change directory
 cd /etc/letsencrypt/live/$1
 
-# Extract key from new certificate
-openssl pkey -in privkey.pem -out portal.key
+# Test if certificate has recently been updated (less than 1 day)
+current=`date +%s`
+modified=`stat -c "%Y" privkey.pem`
+if [ $(($current-$modified)) -lt 86400 ]; then
 
-# Extract convert cert from pem to crt
-cp cert.pem portal.crt
+  # Flag to indicate last time cert was copied
+  echo $(id) > /opt/lastcopied
 
-# Copy cert and key to relayrocket folder
-cp portal.* /usr/local/rocket/etc
+  # Extract key from new certificate
+  openssl pkey -in privkey.pem -out portal.key
 
-# Change permissions on key
-chmod 640 portal.key
+  # Extract convert cert from pem to crt
+  cp cert.pem portal.crt
 
-# Change permissions on cert
-chmod 644 portal.crt
+  # Copy cert and key to relayrocket folder
+  cp portal.* /usr/local/rocket/etc
 
-# Copy files to next relay rocket only if the ftp user is not null
-if [$2 != 'null']; then
-  scp /usr/local/rocket/etc/portal.* $2@$3:/usr/local/rocket/letsencrypt/
+  # Change permissions on key
+  chmod 640 portal.key
+
+  # Change permissions on cert
+  chmod 644 portal.crt
+
+  # Copy files to next relay rocket only if the ftp user is not null
+  if [$2 != 'null']; then
+    scp /usr/local/rocket/etc/portal.* $2@$3:/usr/local/rocket/letsencrypt/
+  fi
 fi
